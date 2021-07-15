212a3bbcc21b314634bb4a8d591c599056ebb1f4
uid11 <uid.11222@gmail.com>
2021-07-15 23:19:56 +0300

    fix: add missed global types

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./global.d.ts" />
+
 import type {OneOrTwoArgs, UnionToIntersection} from './utils';
 
 export * from '../../../e2ed/types';
