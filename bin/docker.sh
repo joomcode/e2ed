610923158fc4187c2bddbf742e28ab454cb58b10
uid11 <uid.11222@gmail.com>
2021-07-28 00:44:35 +0300

    fix: use E2ED_DOCKER_CONCURRENCY inside docker

--- bin/docker.sh
+++ bin/docker.sh
@@ -3,4 +3,4 @@
 DIR="${WORKDIR:-$PWD}"
 DOCKER_IMAGE="${E2ED_DOCKER_IMAGE:-testcafe/testcafe}"
 
-docker run --rm --user 1000 --entrypoint ./bin/withRetries.js -v $DIR:$DIR -w $DIR/node_modules/e2ed -e NODE_PATH=/usr/lib/node_modules -e E2ED_API_ORIGIN=$E2ED_API_ORIGIN -e E2ED_ORIGIN=$E2ED_ORIGIN $DOCKER_IMAGE
+docker run --rm --user 1000 --entrypoint ./bin/withRetries.js -v $DIR:$DIR -w $DIR/node_modules/e2ed -e NODE_PATH=/usr/lib/node_modules -e E2ED_API_ORIGIN=$E2ED_API_ORIGIN -e E2ED_ORIGIN=$E2ED_ORIGIN -e E2ED_DOCKER_CONCURRENCY=$E2ED_DOCKER_CONCURRENCY $DOCKER_IMAGE
--- e2ed/types/index.ts
+++ e2ed/types/index.ts
@@ -2,6 +2,9 @@ export * from './user';
 
 export type Language = 'de' | 'en';
 
+/**
+ * Test metadata type (testId, severity, etc).
+ */
 export type TestMeta = Readonly<{
   testId: string;
 }>;
