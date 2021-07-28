131317c969988f3dead942b26cedf398f63b3d27
uid11 <uid.11222@gmail.com>
2021-07-28 03:27:21 +0300

    chore: update dev dependencies

--- src/package/expect.ts
+++ src/package/expect.ts
@@ -3,5 +3,5 @@ import {Expect} from './utils/Expect';
 /**
  * Wraps a promised value to assertion for further checks.
  */
-export const expect = <A>(actual: Promise<A>, description: string): Assertion<A> =>
+export const expect = <A>(actual: A | Promise<A>, description: string): Assertion<A> =>
   new Expect(actual, description) as unknown as Assertion<A>;
