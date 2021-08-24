71829b3235fd8a04506512d591a7b33901f5ae89
uid11 <uid.11222@gmail.com>
2021-08-24 11:41:26 +0300

    fix: remove undefined from logs; update dependencies

--- src/package/utils/generalLog.ts
+++ src/package/utils/generalLog.ts
@@ -10,7 +10,7 @@ export const generalLog: Log = (message, payload) => {
   const dateTimeInISO = new Date().toISOString();
   const maybeRunLabel = getLabel(process.env.E2ED_RUN_LABEL);
 
-  const printedString = valueToString(payload);
+  const printedString = payload === undefined ? '' : valueToString(payload);
 
   // eslint-disable-next-line no-console
   console.log(`[e2ed][${dateTimeInISO}]${maybeRunLabel} ${message} ${printedString}\n`);
