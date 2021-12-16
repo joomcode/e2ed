072dbcb0b4572a6acaf35aaa4b675f070f3df276
uid11 <uid.11222@gmail.com>
2021-12-16 15:57:10 +0300

    fix: debug scripts (error with supporting E2ED_DEBUG)

--- bin/e2ed.sh
+++ bin/e2ed.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-if [[ -z $E2ED_DEBUG ]]; then
+if [ -z $E2ED_DEBUG ]
+then
   ./bin/runLocal.js "$@"
 else
   node --inspect-brk=0.0.0.0 ./bin/runLocal.js "$@"
--- bin/runInDocker.sh
+++ bin/runInDocker.sh
@@ -7,7 +7,8 @@ then
     cp -r /opt/e2ed ./node_modules
 fi
 
-if [[ -z $E2ED_DEBUG ]]; then
+if [ -z $E2ED_DEBUG ]
+then
   ./node_modules/e2ed/bin/runWithRetries.js
 else
   node --inspect-brk=0.0.0.0 ./node_modules/e2ed/bin/runWithRetries.js
