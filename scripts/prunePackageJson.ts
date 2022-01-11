03725b2d6682ce56f569c69515feb84dbed499f4
uid11 <uid.11222@gmail.com>
2022-01-11 10:48:12 +0300

    feat: remove field "scripts" from published package.json

---
+++ scripts/prunePackageJson.ts
@@ -0,0 +1,18 @@
+/**
+ * @file Generate pruned package.json for npm package.
+ */
+
+import {writeFileSync} from 'fs';
+import {join} from 'path';
+
+import pkg from '../package.json';
+
+const prunedPackageJsonPath = join(__dirname, 'node_modules', 'e2ed', 'package.json');
+
+const prunedPackageJson: Partial<typeof pkg> = {...pkg};
+
+delete prunedPackageJson.scripts;
+
+const prunedPackageJsonText = JSON.stringify(prunedPackageJson, null, 2);
+
+writeFileSync(prunedPackageJsonPath, prunedPackageJsonText);
