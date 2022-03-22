2db1ec06c54f5b0f448724b83ca91de5c4d8a99b
uid11 <uid.11222@gmail.com>
2022-03-22 11:40:56 +0300

    feat: support skipping of all tests

--- e2ed/hooks/isTestSkipped.ts
+++ e2ed/hooks/isTestSkipped.ts
@@ -6,7 +6,7 @@ import type {TestStaticOptions} from 'e2ed/types';
  * This is the type of the "skipTests" field in the e2ed config,
  * which describes the set of tests that need to be skipped.
  */
-export type SkipTests = readonly string[];
+export type SkipTests = 'all' | readonly string[];
 
 /**
  * This hook is used to determine if a given test should be skipped.
@@ -16,6 +16,10 @@ export const isTestSkipped = (testStaticOptions: TestStaticOptions): boolean => 
   // As with all hooks, you can replace it with your own implementation.
   const {options} = testStaticOptions;
   const {skipTests = []} = getFullConfig();
+
+  if (skipTests === 'all') {
+    return true;
+  }
 
   return skipTests.includes(options.meta.testId);
 };
