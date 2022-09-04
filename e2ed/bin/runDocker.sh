eed6929f28495b29d3aea54e3c8545c01d6616e4
uid11 <uid.11222@gmail.com>
2022-09-04 10:07:52 +0300

    feat: add GitHub Pages with example of report html

--- e2ed/bin/runDocker.sh
+++ e2ed/bin/runDocker.sh
@@ -5,18 +5,14 @@ DEBUG_PORT="${E2ED_DOCKER_DEBUG_PORT:-9229}"
 DIR="${E2ED_WORKDIR:-$PWD}"
 DOCKER_IMAGE=$(grep -m1 dockerImage ./e2ed/config.ts | sed -e "s/^[^'\"\`]*['\"\`]//" -e "s/['\"\`][^'\"\`]*$//")
 MOUNTDIR="${E2ED_MOUNTDIR:-$DIR}"
+PORT=$([ -z $E2ED_DEBUG ] && echo "" || echo "--publish $DEBUG_PORT:$DEBUG_PORT")
 VERSION=$(grep -m1 \"e2ed\": ./package.json | cut -d '"' -f 4)
-
-if [ -z $E2ED_DEBUG ]
-then
-    PORT=""
-else
-    PORT="--publish $DEBUG_PORT:$DEBUG_PORT"
-fi
 
 echo "Run docker image $DOCKER_IMAGE:$VERSION"
 
-docker run --rm $PORT \
+docker run \
+       --rm \
+       $PORT \
        --workdir $DIR \
        --volume $MOUNTDIR:$MOUNTDIR \
        --env E2ED_ORIGIN=$E2ED_ORIGIN \
