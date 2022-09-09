b2a8f5ac9b0229caf14ed8bec594a86f84bbded5
w84v2rhsq4 <yulia@joom.com>
2022-09-09 13:50:28 +0300

    WEB-6040 add assertNumbersAreAprroximatelyEql

---
+++ src/package/actions/asserts/assertNumbersAreApproximatelyEql.ts
@@ -0,0 +1,23 @@
+import {LogEventType} from '../../constants/internal';
+import {log} from '../../utils/log';
+import {expect} from '../../expect';
+
+/**
+ * Checks if two numbers are approximately equal with specified EPS.
+ */
+export const assertNumbersAreApproximatelyEqual = async (
+  firstNumber: number,
+  secondNumber: number,
+  eps = 0.01,
+): Promise<void> => {
+  const diff = Math.abs(firstNumber - secondNumber);
+  const result = diff < eps;
+  // TODO(@uid11): support Smart Assertions in this method after WEB-5031.
+  await log(
+    'Assert that two numbers are approximately equal with specified EPS.',
+    {firstNumber, secondNumber, eps, diff},
+    LogEventType.InternalAssert,
+  );
+
+  await expect(result, 'Two numbers are approximately equal with specified EPS.').ok();
+};
