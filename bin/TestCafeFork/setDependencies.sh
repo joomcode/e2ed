163169bd64123b2d42193fcbab404f69f976fd12
uid11 <uid.11222@gmail.com>
2022-10-23 14:45:01 +0300

    chore: update devDependencies, update testcafe-without-typecheck

--- bin/TestCafeFork/setDependencies.sh
+++ bin/TestCafeFork/setDependencies.sh
@@ -7,5 +7,5 @@ sed -i "s/\x22typescript\x22: \x22[^\x22]*\x22,//" ./bin/TestCafeFork/package/pa
 
 sed -i "s|\x22@types/node\x22: \x22[^\x22]*\x22,||" ./bin/TestCafeFork/package/package.json
 
-sed -i "s|\x22devDependencies\x22|\x22peerDependencies\x22: {\x22@types/node\x22: \x22>=16\x22, \x22pngjs\x22: \x22>=6\x22, \x22typescript\x22: \x22>=4\x22},\x22devDependencies\x22|" ./bin/TestCafeFork/package/package.json
+sed -i "s|\x22devDependencies\x22|\x22peerDependencies\x22: {\x22@types/node\x22: \x22>=18\x22, \x22pngjs\x22: \x22>=6\x22, \x22typescript\x22: \x22>=4\x22},\x22devDependencies\x22|" ./bin/TestCafeFork/package/package.json
 
