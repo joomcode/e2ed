abcd5ed9c737a494ca0d94f86d60ede634c5de05
uid11 <uid.11222@gmail.com>
2022-12-20 07:32:41 +0300

    fix: add bin-v8-flags-filter to main dependencies

--- src/bin/localEntrypoint.ts
+++ src/bin/localEntrypoint.ts
@@ -1,6 +1,5 @@
 import {join} from 'node:path';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
 import v8FlagsFilter from 'bin-v8-flags-filter';
 
 import {INSTALLED_E2ED_DIRECTORY_PATH} from '../constants/internal';
