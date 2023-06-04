2180cf490be7a2c72f47db21ef29c02e61f9c6b7
uid11 <uid.11222@gmail.com>
2023-06-04 06:31:26 +0300

    feat: kill TestCafe processes occupying ports after each retry

--- src/bin/runTestsSubprocess.ts
+++ src/bin/runTestsSubprocess.ts
@@ -1,6 +1,11 @@
+import {getFullPackConfig} from '../utils/getFullPackConfig';
 import {runTests} from '../utils/tests';
 
 import type {RunRetryOptions} from '../types/internal';
+
+const {testIdleTimeout} = getFullPackConfig();
+
+setInterval(() => process.send?.(null), testIdleTimeout);
 
 /**
  * Returns exit code 0, if all tests passed, and 1 otherwise.
