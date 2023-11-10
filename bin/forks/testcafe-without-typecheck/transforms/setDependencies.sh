45e999b26622ef1f2fc50b84e1c5bdda48344ae1
uid11 <uid.11222@gmail.com>
2023-11-10 05:21:52 +0300

    FI-646 chore: update TestCafe to 3.4.0

--- bin/forks/testcafe-without-typecheck/transforms/setDependencies.sh
+++ bin/forks/testcafe-without-typecheck/transforms/setDependencies.sh
@@ -5,7 +5,7 @@ sed -i "s|\x22@types/node\x22: \x22[^\x22]*\x22,||" ./bin/forks/testcafe-without
 sed -i "s/\x22bin-v8-flags-filter\x22: \x22[^\x22]*\x22,/\x22bin-v8-flags-filter\x22: \x221.2.0\x22,/" ./bin/forks/testcafe-without-typecheck/package/package.json
 sed -i "s/\x22globby\x22: \x22[^\x22]*\x22,/\x22globby\x22: \x2211.1.0\x22,/" ./bin/forks/testcafe-without-typecheck/package/package.json
 sed -i "s/\x22pngjs\x22: \x22[^\x22]*\x22,/\x22pngjs\x22: \x227.0.0\x22,/" ./bin/forks/testcafe-without-typecheck/package/package.json
-sed -i "s/\x22testcafe-hammerhead\x22: \x22[^\x22]*\x22,/\x22testcafe-hammerhead-up\x22: \x2231.6.1-rc.1\x22,/" ./bin/forks/testcafe-without-typecheck/package/package.json
+sed -i "s/\x22testcafe-hammerhead\x22: \x22[^\x22]*\x22,/\x22testcafe-hammerhead-up\x22: \x2231.6.3-rc.1\x22,/" ./bin/forks/testcafe-without-typecheck/package/package.json
 sed -i "s|\x22testcafe-reporter-spec\x22: \x22[^\x22]*\x22,|\x22testcafe-reporter-for-e2ed\x22: \x220.0.4\x22,|" ./bin/forks/testcafe-without-typecheck/package/package.json
 sed -i "s/\x22typescript\x22: \x22[^\x22]*\x22,//" ./bin/forks/testcafe-without-typecheck/package/package.json
 
