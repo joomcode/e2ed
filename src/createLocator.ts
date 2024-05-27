56d2f47163e53425bd8fde761e5c5a4574f0620e
uid11 <uid.11222@gmail.com>
2024-05-27 06:48:13 +0300

    chore: update `create-locator` and `devtools-protocol`

--- src/createLocator.ts
+++ src/createLocator.ts
@@ -28,6 +28,6 @@ export {
 } from 'create-locator';
 export type {PARAMETERS} from 'create-locator/oldTypes';
 // eslint-disable-next-line import/no-internal-modules
-export {createLocatorCreatorInTests} from 'create-locator/createLocatorCreatorInTests';
+export {createSelectorFunctions} from 'create-locator/createSelectorFunctions';
 // eslint-disable-next-line import/no-internal-modules
 export {getCssSelectorFromAttributesChain} from 'create-locator/getCssSelectorFromAttributesChain';
